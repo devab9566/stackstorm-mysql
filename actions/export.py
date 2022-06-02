f297f13177ac4c814fb97b642281d420f010385a
devab9566 <106380838+devab9566@users.noreply.github.com>
2022-06-02 16:29:59 +0530

    Update export.py

--- actions/export.py
+++ actions/export.py
@@ -1,18 +1,19 @@
-import MySQLdb as dbapi
-import sys
-import csv
+import MYSQLdb
+import mysql.connector
+import pandas as pd
 
-dbServer='localhost'
-dbPass='supersecretpassword'
-dbSchema='dbTest'
-dbUser='root'
 
-dbQuery='SELECT * FROM pbTest.Orders;'
 
-db=dbapi.connect(host=dbServer,user=dbUser,passwd=dbPass)
-cur=db.cursor()
-cur.execute(dbQuery)
-result=cur.fetchall()
+mydb = mysql.connector.connect(
+        host="localhost",
+        port=1234,
+        user="root",
+        passwd="1234",
+        db="db",
 
-c = csv.writer(open("temp.csv","wb"))
-c.writerow(result)pymysql.err.OperationalError: (2003, "Can't connect to MySQL server on 'localhost:8080' ([Errno 11001] getaddrinfo failed)")
+query= ('SELECT * FROM db')
+
+mycursor=mydb.cursor
+mydb = mycursor.fetchall()
+df = pd.DataFrame(mydb)
+df_csv = df.to_csv("G:\sample.csv" )
